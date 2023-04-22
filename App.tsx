59387b8d7e8e275c5baf3ca6828a2621a686a247
lul <luan.melo.bravo10@gmail.com>
2023-04-22 11:20:52 -0300

    tailwind theme set & images added to project

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
     return (
-        <View className='bg-slate-950 flex-1 justify-center items-center'>
+        <View className='bg-cBeige flex-1 justify-center items-center'>
             <Text className='text-slate-50 p-6 font-bold'>LUL Starting Expo Template</Text>
             <Image className='w-[400] h-[400]' source={require('./source/assets/maxwell-the-cat.gif')}/>
             <StatusBar style="auto" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./source/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+        'cBeige': '#E2E6C6',
+        'cLightGreen': '#9BAC45',
+        'cDarkGreen': '#82932C',
+        'cDarkerGreen': '#4D5328',
+    },
     extend: {},
   },
   plugins: [],
