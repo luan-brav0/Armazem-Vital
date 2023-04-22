fb8946b9cd46c22797b57760564cc44a8ca66315
lul <luan.melo.bravo10@gmail.com>
2023-04-22 14:10:21 -0300

    Main.tsx

---
+++ source/pages/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Image } from 'expo-image'
+
+
+const Main: React.FC = () => {
+    return (
+        <View>
+            <Image source={require('')} />
+        </View>
+    );
+}
+
+export default Main;
